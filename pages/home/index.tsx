e00be57e481be5e9f3fffc9eafd459b9e7558e0e
imduchuyyy <duchuy.124dk@gmail.com>
2024-06-03 13:01:44 +0700

    chore: Add receive button to home page

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -44,6 +44,9 @@ export const Home = () => {
                 <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                     Send
                 </button>
+                <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Receive
+                </button>
             </div>
         </div>
     )
