8ebef324f8f5c3ca575a90f92f5b197142c11920
imduchuyyy <duchuy.124dk@gmail.com>
2024-06-03 12:58:04 +0700

    chore: Refactor home page component to improve readability and maintainability

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,22 +1,27 @@
-import React, { useEffect, useState } from "react";
-import { Hex, PublicClient } from "viem";
-import { AccountService } from "~account";
-import { useWalletState } from "~states";
+import React, { useEffect, useState } from "react"
+import { Hex, PublicClient } from "viem"
+
+import { AccountService } from "~account"
+import { useWalletState } from "~states"
 
 export const Home = () => {
-    const [address, setAddress] = useState<Hex>("0x");
-    const [balance, setBalance] = useState<string>("");
-    const account: AccountService = useWalletState((state: any) => state.account)
-    const ethClient: PublicClient = useWalletState((state: any) => state.ethClient)
+    const [address, setAddress] = useState<Hex>("0x")
+    const [balance, setBalance] = useState<string>("")
+    const account: AccountService = useWalletState(
+        (state: any) => state.account
+    )
+    const ethClient: PublicClient = useWalletState(
+        (state: any) => state.ethClient
+    )
 
     const fetchAddress = async () => {
         const address = await account.getSender()
-        setAddress(address);
+        setAddress(address)
     }
 
     const fetchBalance = async () => {
         if (address != "0x") {
-            const balance = await ethClient.getBalance({address})
+            const balance = await ethClient.getBalance({ address })
             setBalance(balance.toString())
         }
     }
@@ -30,9 +35,16 @@ export const Home = () => {
     }, [address])
 
     return (
-        <div>
-            <div>Wallet address: {address}</div>
-            <div>Wallet balance: {balance} VIC</div>
+        <div className="p-6 max-w-sm mx-auto rounded-xl shadow-md flex items-center space-x-4">
+            <div>
+                <div className="font-medium">
+                    Wallet address: {address}
+                </div>
+                <p className="text-gray-500">Wallet balance: {balance} VIC</p>
+                <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Send
+                </button>
+            </div>
         </div>
     )
 }
