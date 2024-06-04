d3b7c42cc0ac20c18ccb4484ce4d17ccbc99666c
imduchuyyy <duchuy.124dk@gmail.com>
2024-06-04 23:41:03 +0700

    chore: Add localforage dependency for wallet storage

---
+++ stores/types.ts
@@ -0,0 +1,30 @@
+import { Hex } from "viem";
+
+export interface IWallet {
+    id: number
+    name: string
+    address: Hex
+}
+
+export interface IWalletStoreState {
+    activeWallet: number
+    wallets: IWallet[]
+    onCreateWallet: (wallet: IWallet) => void
+    setActiveWallet: (id: number) => void
+}
+
+export interface IUserCredentials {
+    password?: string;
+    encryptedMnemonic?: string
+}
+
+export interface IUserStoreState {
+    credentials?: IUserCredentials;
+    onSetCredentials: (credentials: IUserCredentials) => void;
+    initMnemonic: (mnemonic: string) => void;
+}
+
+export interface ICacheStoreState {
+    cachePassword?: string;
+    onInitCache: (password: string) => void;
+}
---
+++ stores/walletStore.ts
@@ -0,0 +1,29 @@
+import localforage from "localforage"
+import { create } from "zustand"
+import { createJSONStorage, persist } from "zustand/middleware"
+
+import { IWallet, IWalletStoreState } from "./types"
+
+export const useWalletStore = create<IWalletStoreState>()(
+    persist(
+        (set) => ({
+            wallets: [],
+            activeWallet: 0,
+            onCreateWallet: (wallet: IWallet) => {
+                set((state) => ({
+                    wallets: [...state.wallets, wallet],
+                    activeWallet: wallet.id
+                }))
+            },
+            setActiveWallet: (id: number) => {
+                set((state) => ({
+                    activeWallet: id
+                }))
+            }
+        }),
+        {
+            name: "wallet-store",
+            storage: createJSONStorage(() => localforage)
+        }
+    )
+)
