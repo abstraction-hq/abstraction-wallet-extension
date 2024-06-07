5306e6c99ed148e0e9985ac52bd90cbfd16e8a1b
imduchuyyy <duchuy.124dk@gmail.com>
2024-06-07 23:13:36 +0700

    injected script

--- background/index.ts
+++ background/index.ts
@@ -1,5 +1,10 @@
 async function main() {
-    // (window as any).abstraction = "hello_world"
+    // handle requests from injected script
+    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+        console.log(`Received request: ${request.method} with params: ${request.params}`);
+        // handle request
+        sendResponse({ result: "success" });
+    });
 }
 
 main()
--- contents/injectedScript.ts
+++ contents/injectedScript.ts
@@ -1,6 +1,24 @@
-const AbstractionWallet = {
-    walletName: "abstraction"
+interface IRequest {
+    readonly method: string
+    readonly params?: readonly unknown[] | object
 }
 
-// @ts-expect-error
-window.abstraction = AbstractionWallet;
+const AbstractionWallet: Record<string, any> = {
+    walletName: "abstraction",
+    request: async ({ method, params = [] }: IRequest) => {
+        // @ts-ignore
+        return new Promise((resolve, reject) => {
+            // Here you would handle different methods. This is just a placeholder.
+            console.log(`Handling request: ${method} with params: ${params}`)
+            window.postMessage({ method, params }, window.location.origin)
+
+            window.addEventListener("message", async (event) => {
+                console.log(`Received response: ${event}`)
+                resolve("Hello from AbstractionWallet!")
+            })
+        })
+    }
+}
+
+// @ts-ignore
+window.abstraction = AbstractionWallet
