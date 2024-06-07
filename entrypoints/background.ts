325d6fd02f4ff6c96a846c6efcd39a78c4dd9fe8
imduchuyyy <duchuy.124dk@gmail.com>
2024-06-07 22:37:12 +0700

    chore: Refactor background script to use browser.tabs.query for improved functionality

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,13 +1,16 @@
 async function getTabId() {
-    const currentTab = await browser.tabs.getCurrent();
+    const currentTab = await browser.tabs.query({
+        currentWindow: true,
+    });
     console.log(currentTab)
-    return browser.tabs.getCurrent().then(tab => tab.id);
+    return currentTab[0].id;
 }
 export default defineBackground(() => {
     console.log('Hello background!', { id: browser.runtime.id });
     getTabId().then((tabId) => {
+        console.log(tabId)
         browser.scripting.executeScript({
-                target : {tabId: tabId as number, allFrames: true},
+                target : {tabId: tabId as number},
                 files : [ "injected.js" ],
             }).then(() => console.log("script injected on target frames"));
     });
